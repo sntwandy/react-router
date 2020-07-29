2e5ee34a95331a6e954b88aebb70561eb71c759e
sntwandy <sntwandy@gmail.com>
2020-07-29 11:01:07 -0400

    Manipulating the history and location

--- src/pages/components/not-found.js
+++ src/pages/components/not-found.js
@@ -4,10 +4,27 @@ import React, { PureComponent } from 'react';
 import './generic-page.css';
 
 class NotFound extends PureComponent {
+
+  handleRandomClick = () => {
+    const random = Math.round(Math.random() * (10 - 1) + 1);
+    this.props.history.push(`/videos?id=${random}`);
+  };
+
+  handleForwardClick = () => {
+    this.props.history.goForward();
+  };
+
+  handleBackClick = () => {
+    this.props.history.goBack();
+  };
+
   render() {
     return(
       <div className="page-not-found">
         <h1>Error 404 Not Found</h1>
+        <button className="Button" onClick={this.handleBackClick}>Back</button>
+        <button className="Button" onClick={this.handleRandomClick}>Random video</button>
+        <button className="Button" onClick={this.handleForwardClick}>Next</button>
       </div>
     );
   };
--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './search.css';
+import { Prompt } from 'react-router';
 // function Search(props) {
 //   return (
 //     <form action=""></form>
@@ -9,8 +10,11 @@ import './search.css';
 const Search = (props) => (
   <form
     className="Search"
-    onSubmit={props.handleSubmit}
-  >
+    onSubmit={props.handleSubmit}>
+    <Prompt
+      when={props.prompt}
+      message= "Are you sure to go out the page?"
+    />
     <input
       ref={props.setRef}
       type="text"
